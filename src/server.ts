0ea762160a8e62b3fb85df4678d35a73e8690254
oaslan16 <oaslan16@ku.edu.tr>
2020-10-05 14:34:08 +0300

    bug fix

--- src/server.ts
+++ src/server.ts
@@ -7,22 +7,9 @@ import { warn } from "loglevel";
 import path from "path";
 
 // Start Express server
-let server: http.Server | https.Server;
-if (process.env.NODE_ENV === "production") {
-    const certOptions = {
-        key: fs.readFileSync(path.resolve("./ssl/kumunite.key")),
-        cert: fs.readFileSync(path.resolve("./ssl/kumunite.crt")),
-        ca: fs.readFileSync(path.resolve("./ssl/kumunite.ca-bundle")),
-    };
-    server = https.createServer(certOptions, app).listen(process.env.PORT, () => {
-        warn("  App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"));
-        warn("\tPress CTRL-C to stop\n");
-    });
-} else {
-    server = http.createServer(app).listen(process.env.PORT, () => {
-        warn("  App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"));
-        warn("\tPress CTRL-C to stop\n");
-    });
-}
+let server = http.createServer(app).listen(process.env.PORT, () => {
+    warn("  App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"));
+    warn("\tPress CTRL-C to stop\n");
+});
 
 export default server;
