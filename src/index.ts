dad3c7a9dd819276ef8a10198ede1f06a89aea71
oaslan16 <oaslan16@ku.edu.tr>
2020-10-05 13:16:49 +0300

    error logging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as Discord from "discord.js";
-import { MessageAttachment } from "discord.js";
+import { MessageAttachment, Message } from "discord.js";
 
 const client = new Discord.Client();
 const prefix = "!";
@@ -8,8 +8,7 @@ const prefix = "!";
     try {
         await client.login(process.env.BOT_TOKEN);
 
-        client.on("message", async function (message) {
-            console.log(message);
+        client.on("message", async function (message: Message) {
             if (message.author.bot) return;
             if (!message.content.startsWith(prefix)) return;
 
@@ -18,13 +17,19 @@ const prefix = "!";
             const command = args.shift()!.toLowerCase();
 
             if (command === "ping") {
-                const timeTaken = Date.now() - message.createdTimestamp;
-                await message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+                try {
+                    const timeTaken = Date.now() - message.createdTimestamp;
+                    await message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+                } catch (error) {
+                    await message.reply(`!!ERROR: ${error.message}`);
+                }
             } else if (message.content === "!rip") {
-                // Create the attachment using MessageAttachment
-                const attachment = new MessageAttachment("https://i.imgur.com/w3duR07.png");
-                // Send the attachment in the message channel
-                await message.channel.send(attachment);
+                try {
+                    const attachment = new MessageAttachment("https://i.imgur.com/w3duR07.png");
+                    await message.channel.send(attachment);
+                } catch (error) {
+                    await message.reply(`!!ERROR: ${error.message}`);
+                }
             }
         });
 
