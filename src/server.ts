b3435608c6f32c65d180a682767548c5a955f215
oaslan16 <oaslan16@ku.edu.tr>
2020-10-13 21:45:36 +0300

    Wellcome message

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import app from "./app";
-import * as fs from "fs";
-import * as https from "https";
 import * as http from "http";
 import { warn } from "loglevel";
-
-import path from "path";
 
 // Start Express server
 let server = http.createServer(app).listen(process.env.PORT, () => {
--- src/utils/discordUtil.ts
+++ src/utils/discordUtil.ts
@@ -1,5 +1,5 @@
 import * as Discord from "discord.js";
-import { MessageAttachment, Message } from "discord.js";
+import { MessageAttachment, Message, MessageEmbed, Channel } from "discord.js";
 
 const client = new Discord.Client();
 const prefix = "!";
@@ -16,7 +16,7 @@ export const initialize = async () => {
             const args = commandBody.split(" ");
             const command = args.shift()!.toLowerCase();
 
-            if (command === "ping") {
+            if (command === "test") {
                 try {
                     const timeTaken = Date.now() - message.createdTimestamp;
                     await message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
@@ -31,11 +31,50 @@ export const initialize = async () => {
                     await message.reply(`!!ERROR: ${error.message}`);
                 }
             }
+
+            if (message.content.startsWith("!kick")) {
+                const user = message.mentions.users.first();
+                if (user) {
+                    const member = message.guild != null ? message.guild.member(user) : null;
+                    if (member) {
+                        member
+                            .kick("Optional reason that will display in the audit logs")
+                            .then(() => {
+                                message.reply(`Successfully kicked ${user.tag}`);
+                            })
+                            .catch((err) => {
+                                message.reply("I was unable to kick the member");
+                                console.error(err);
+                            });
+                    } else {
+                        await message.reply("That user isn't in this guild!");
+                    }
+                } else {
+                    await message.reply("You didn't mention the user to kick!");
+                }
+            }
         });
 
         client.on("ready", () => {
             console.log(`Logged in as ${client.user!.tag}!`);
         });
+
+        client.on("guildMemberAdd", async (member) => {
+            try {
+                let roles = await member.guild.roles.fetch();
+                let newbieRole = roles.cache.find((r) => {
+                    return r.name == "newbie";
+                });
+                if (newbieRole != null) {
+                    await member.roles.add(newbieRole, "New Members");
+                }
+                member.send(`Welcome to the server, ${member}`);
+            } catch (e) {
+                console.log(e.message);
+            }
+        });
+
+        client.on("message", async (message: Message) => {});
     } catch (error) {
         console.log(error.message);
     }
