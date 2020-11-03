477dbaec31c8e595b784e5bcc2a24e70ed1e8f87
Okan Aslan <okanaslan.08@gmail.com>
2020-11-03 21:25:18 +0300

    removing server dependencies

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import * as Discord from "discord.js";
+import { Message, GuildMember, PartialGuildMember } from "discord.js";
+import { discordUtil } from "./utils/discordUtil";
+
+const client = new Discord.Client();
+const prefix = "!";
+
+// eslint-disable-next-line no-unused-vars
+const initialize = async () => {
+    try {
+        await client.login(process.env.BOT_TOKEN);
+
+        client.on("message", async (message: Message) => {
+            if (message.author.bot) return;
+            if (!message.content.startsWith(prefix)) return;
+
+            if (message.content.startsWith("!addRole")) {
+                discordUtil.createRole();
+            }
+
+            if (message.content.startsWith("!addMajor")) {
+                discordUtil.addRole();
+            }
+
+            if (message.content.startsWith("!addMinor")) {
+                discordUtil.addRole();
+            }
+        });
+
+        client.on("ready", () => {
+            console.log(`Logged in as ${client.user!.tag}!`);
+        });
+
+        client.on("guildMemberAdd", async (member: GuildMember | PartialGuildMember) => {
+            try {
+                const roles = await member.guild.roles.fetch();
+                const newbieRole = roles.cache.find((r) => r.name === "newbie");
+                if (newbieRole != null) {
+                    await member.roles.add(newbieRole, "New Members");
+                }
+                await member.send(`Welcome to the server, ${member}`);
+            } catch (error) {
+                console.log(error.message);
+            }
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
